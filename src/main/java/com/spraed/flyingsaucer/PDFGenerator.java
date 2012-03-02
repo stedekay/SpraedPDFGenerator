37c794e7023a80236b540493818c4321a33bdf73
Stefan Blanke <stefblanke@gmail.com>
2012-03-02 15:01:19 +0100

    multiple pdfs can be printed in one stack

--- src/main/java/com/spraed/flyingsaucer/PDFGenerator.java
+++ src/main/java/com/spraed/flyingsaucer/PDFGenerator.java
@@ -21,52 +21,67 @@ public class PDFGenerator {
 
 	public static void main(String[] args) throws IOException,
 			DocumentException, ParseException {
-		
+
 		// get options
 		Options options = new Options();
 		options.addOption("html", true, "HTML path");
 		options.addOption("pdf", true, "PDF path");
 		options.addOption("encoding", true, "Encoding");
-		
+
 		CommandLineParser parser = new PosixParser();
 		CommandLine cmd = parser.parse(options, args);
 
-		String htmlFile = cmd.getOptionValue("html");
+		String htmlFiles = cmd.getOptionValue("html");
 		String pdfFile = cmd.getOptionValue("pdf");
 		String encoding = cmd.getOptionValue("encoding");
-		
-		// get file input stream
-		String htmlInputFile = htmlFile;
-		InputStream htmlInputStream = new FileInputStream(htmlInputFile);
-		
-		// get file output stream for a tidy html
-		File htmlOutputFile = File.createTempFile("tidy", ".html");
-		OutputStream htmlOutputStream = new FileOutputStream(htmlOutputFile);
-		
-		// clean up html
-		Tidy tidy = new Tidy();
-		tidy.setXHTML(true);
-		tidy.setHideComments(true);
-		tidy.setShowWarnings(false);
-		tidy.setQuiet(true);
-		tidy.setInputEncoding(encoding);
-		tidy.setOutputEncoding(encoding);
-		tidy.parse(htmlInputStream, htmlOutputStream);
+
+		String[] files = htmlFiles.split(", ");
 
 		// get output stream
 		OutputStream os = new FileOutputStream(pdfFile);
-
-		// render pdf from tidy html
-		ITextRenderer renderer = new ITextRenderer();
-		renderer.setDocument(htmlOutputFile);
-		renderer.layout();
-		renderer.createPDF(os);
-
-		os.close();
 		
-		// delete tidy html file
-		htmlInputStream.close();
-		htmlOutputStream.close();
-		htmlOutputFile.delete();
+		// create pdf renderer
+		ITextRenderer renderer = new ITextRenderer();
+
+		for (int i = 0; i < files.length; i++) {
+			// get file input stream
+			String htmlInputFile = files[i];
+			InputStream htmlInputStream = new FileInputStream(htmlInputFile);
+
+			// get file output stream for a tidy html
+			File htmlOutputFile = File.createTempFile("tidy", ".html");
+			OutputStream htmlOutputStream = new FileOutputStream(htmlOutputFile);
+
+			// clean up html
+			Tidy tidy = new Tidy();
+			tidy.setXHTML(true);
+			tidy.setHideComments(true);
+			tidy.setShowWarnings(false);
+			tidy.setQuiet(true);
+			tidy.setInputEncoding(encoding);
+			tidy.setOutputEncoding(encoding);
+			tidy.parse(htmlInputStream, htmlOutputStream);
+			
+			// close html input and output stream
+			htmlInputStream.close();
+			htmlOutputStream.close();
+
+			// render pdf from tidy html
+			renderer.setDocument(htmlOutputFile);
+			renderer.layout();
+			
+			if(i == 0) {
+				renderer.createPDF(os, false);
+			} else {
+				renderer.writeNextDocument();
+			}
+			
+			// delete html output file
+			htmlOutputFile.delete();
+		}
+		
+		renderer.finishPDF();
+		os.close();
+
 	}
 }

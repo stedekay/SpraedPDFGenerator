adb662c98d0c5f706238628c2177ea7fe9b41180
Stefan Blanke <blanke@enterio.de>
2014-07-10 12:09:15 +0200

    forcing output due to word pseudo tags

--- src/main/java/com/spraed/flyingsaucer/PDFGenerator.java
+++ src/main/java/com/spraed/flyingsaucer/PDFGenerator.java
@@ -17,23 +17,22 @@ import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
+import org.w3c.dom.DOMException;
 import org.w3c.tidy.Tidy;
-import org.xhtmlrenderer.pdf.ITextRenderer;
-//import org.xhtmlrenderer.util.XRLog;
-
 import org.xhtmlrenderer.pdf.ITextFontResolver;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import com.lowagie.text.DocumentException;
-import org.w3c.dom.DOMException;
 
 public class PDFGenerator {
 
 	public static void main(String[] args) throws IOException,
 			DocumentException, ParseException {
-		
+
 		// enable logging
-//		System.getProperties().setProperty("xr.util-logging.loggingEnabled", "true");
-//		XRLog.setLoggingEnabled(true);
+		// System.getProperties().setProperty("xr.util-logging.loggingEnabled",
+		// "true");
+		// XRLog.setLoggingEnabled(true);
 
 		// get options
 		Options options = new Options();
@@ -52,25 +51,26 @@ public class PDFGenerator {
 
 		// read html file directories from tmp file
 		InputStream tmpInputStream = new FileInputStream(tmpFile);
-		
+
 		InputStream dataStream = new DataInputStream(tmpInputStream);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(dataStream));
-		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				dataStream));
+
 		List<String> htmlFiles = new ArrayList<String>();
 		String strLine;
-		while((strLine = reader.readLine()) != null) {
+		while ((strLine = reader.readLine()) != null) {
 			htmlFiles.add(strLine);
 		}
-		
+
 		// close buffered reader
 		reader.close();
-		
+
 		// get output stream
 		OutputStream os = new FileOutputStream(pdfFile);
-		
+
 		// create pdf renderer
 		ITextRenderer renderer = new ITextRenderer();
-                
+
 		if (fontPaths != null) {
 			configureFonts(renderer, fontPaths.split(","));
 		}
@@ -84,16 +84,23 @@ public class PDFGenerator {
 			File htmlOutputFile = File.createTempFile("tidy", ".html");
 			OutputStream htmlOutputStream = new FileOutputStream(htmlOutputFile);
 
-			// clean up html
+			// clean up HTML with jTidy
 			Tidy tidy = new Tidy();
 			tidy.setXHTML(true);
 			tidy.setHideComments(true);
 			tidy.setShowWarnings(false);
 			tidy.setQuiet(true);
+			tidy.setForceOutput(true);
+
+//			// add unknown tags
+//			Properties props = new Properties();
+//			props.setProperty("new-empty-tags", "o:p");
+//			tidy.setConfigurationFromProps(props);
+
 			tidy.setInputEncoding(encoding);
 			tidy.setOutputEncoding(encoding);
 			tidy.parse(htmlInputStream, htmlOutputStream);
-			
+
 			// close html input and output stream
 			htmlInputStream.close();
 			htmlOutputStream.close();
@@ -101,31 +108,33 @@ public class PDFGenerator {
 			// render pdf from tidy html
 			renderer.setDocument(htmlOutputFile);
 			renderer.layout();
-			
+
 			// check if pdf has to be created, iterate index afterwards
-			if(index == 0) {
+			if (index == 0) {
 				renderer.createPDF(os, false);
 			} else {
 				renderer.writeNextDocument();
 			}
 			index++;
-			
+
 			// delete html output file
 			htmlOutputFile.delete();
 		}
-		
+
 		renderer.finishPDF();
 		os.close();
 
 	}
-        
-	protected static void configureFonts(ITextRenderer renderer, String[] fontPaths) throws DOMException,
-		DocumentException, IOException {
-            
+
+	protected static void configureFonts(ITextRenderer renderer,
+			String[] fontPaths) throws DOMException, DocumentException,
+			IOException {
+
 		ITextFontResolver resolver = renderer.getFontResolver();
-            
+
 		for (String fontPath : fontPaths) {
 			resolver.addFont(fontPath, true);
 		}
 	}
+
 }

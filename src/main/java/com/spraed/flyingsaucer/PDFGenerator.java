e3bd5463451a933fe5f3d25af47156353b74a31b
Stefan Blanke <blanke@enterio.de>
2021-07-02 13:50:00 +0200

    Replace invalid xml characters

--- src/main/java/com/spraed/flyingsaucer/PDFGenerator.java
+++ src/main/java/com/spraed/flyingsaucer/PDFGenerator.java
@@ -1,6 +1,7 @@
 package com.spraed.flyingsaucer;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -9,6 +10,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,35 +31,12 @@ import com.lowagie.text.DocumentException;
 
 public class PDFGenerator {
 
-	public static void main(String[] args) throws IOException,
-			DocumentException, ParseException {
+	public static void main(String[] args) throws IOException, DocumentException, ParseException {
+		CommandLine cmd = defineOptions(args);
 
-		// enable logging
-		// System.getProperties().setProperty("xr.util-logging.loggingEnabled",
-		// "true");
-		// XRLog.setLoggingEnabled(true);
-
-		// get options
-		Options options = new Options();
-		options.addOption("html", true, "HTML path");
-		options.addOption("pdf", true, "PDF path");
-		options.addOption("encoding", true, "Encoding");
-		options.addOption("fontPaths", true, "CSV font paths");
-
-		CommandLineParser parser = new PosixParser();
-		CommandLine cmd = parser.parse(options, args);
-
-		String tmpFile = cmd.getOptionValue("html");
-		String pdfFile = cmd.getOptionValue("pdf");
-		String encoding = cmd.getOptionValue("encoding");
-		String fontPaths = cmd.getOptionValue("fontPaths");
-
-		// read html file directories from tmp file
-		InputStream tmpInputStream = new FileInputStream(tmpFile);
-
+		InputStream tmpInputStream = new FileInputStream(cmd.getOptionValue("html"));
 		InputStream dataStream = new DataInputStream(tmpInputStream);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(
-				dataStream));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(dataStream));
 
 		List<String> htmlFiles = new ArrayList<String>();
 		String strLine;
@@ -62,65 +44,31 @@ public class PDFGenerator {
 			htmlFiles.add(strLine);
 		}
 
-		// close buffered reader
 		reader.close();
 
-		// get output stream
-		OutputStream os = new FileOutputStream(pdfFile);
-
-		// create pdf renderer
+		OutputStream os = new FileOutputStream(cmd.getOptionValue("pdf"));
 		ITextRenderer renderer = new ITextRenderer();
 
+		String fontPaths = cmd.getOptionValue("fontPaths");
 		if (fontPaths != null) {
 			configureFonts(renderer, fontPaths.split(","));
 		}
 
 		int index = 0;
 		for (String htmlInputFile : htmlFiles) {
-			// get file input stream
-			InputStream htmlInputStream = new FileInputStream(htmlInputFile);
-
-			// get file output stream for a tidy html
-			File htmlOutputFile = File.createTempFile("tidy", ".html");
-			OutputStream htmlOutputStream = new FileOutputStream(htmlOutputFile);
-
-			// clean up HTML with jTidy
-			Tidy tidy = new Tidy();
-			tidy.setXHTML(true);
-			tidy.setHideComments(true);
-			tidy.setShowWarnings(false);
-			tidy.setQuiet(true);
-			tidy.setForceOutput(true);
-
-//			// add unknown tags
-//			Properties props = new Properties();
-//			props.setProperty("new-empty-tags", "o:p");
-//			tidy.setConfigurationFromProps(props);
-
-			tidy.setInputEncoding(encoding);
-			tidy.setOutputEncoding(encoding);
-			tidy.parse(htmlInputStream, htmlOutputStream);
-
-			// close html input and output stream
-			htmlInputStream.close();
-			htmlOutputStream.close();
+			File htmlOutputFile = tidyHtml(htmlInputFile, cmd.getOptionValue("encoding"));
 
 			try {
-				// render pdf from tidy html
 				renderer.setDocument(htmlOutputFile);
 				renderer.layout();
 
-				// check if pdf has to be created, iterate index afterwards
 				if (index == 0) {
 					renderer.createPDF(os, false);
 				} else {
 					renderer.writeNextDocument();
 				}
 				index++;
-			}
-
-			finally {
-				// delete html output file
+			} finally {
 				htmlOutputFile.delete();
 			}
 		}
@@ -130,9 +78,8 @@ public class PDFGenerator {
 
 	}
 
-	protected static void configureFonts(ITextRenderer renderer,
-			String[] fontPaths) throws DOMException, DocumentException,
-			IOException {
+	protected static void configureFonts(ITextRenderer renderer, String[] fontPaths)
+			throws DOMException, DocumentException, IOException {
 
 		ITextFontResolver resolver = renderer.getFontResolver();
 
@@ -141,4 +88,47 @@ public class PDFGenerator {
 		}
 	}
 
+	private static CommandLine defineOptions(String[] args) throws ParseException {
+		Options options = new Options();
+		options.addOption("html", true, "HTML path");
+		options.addOption("pdf", true, "PDF path");
+		options.addOption("encoding", true, "Encoding");
+		options.addOption("fontPaths", true, "CSV font paths");
+
+		CommandLineParser parser = new PosixParser();
+		CommandLine cmd = parser.parse(options, args);
+
+		return cmd;
+	}
+
+	private static File tidyHtml(String htmlInputFile, String encoding) throws IOException {
+		String content = readFile(htmlInputFile, StandardCharsets.UTF_8);
+		content = content.replaceAll("[^\\x00-\\x7F]", "");
+		
+		InputStream htmlInputStream = new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
+		File htmlOutputFile = File.createTempFile("tidy", ".html");
+		OutputStream htmlOutputStream = new FileOutputStream(htmlOutputFile);
+
+		Tidy tidy = new Tidy();
+		tidy.setXHTML(true);
+		tidy.setHideComments(true);
+		tidy.setShowWarnings(false);
+		tidy.setQuiet(true);
+		tidy.setForceOutput(true);
+		tidy.setWord2000(true);
+
+		tidy.setInputEncoding(encoding);
+		tidy.setOutputEncoding(encoding);
+		tidy.parse(htmlInputStream, htmlOutputStream);
+
+		htmlInputStream.close();
+		htmlOutputStream.close();
+
+		return htmlOutputFile;
+	}
+
+	private static String readFile(String path, Charset encoding) throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		return new String(encoded, encoding);
+	}
 }

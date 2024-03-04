fdc2b499b816bdfaea9fa8a59fb1630eaeaf44c2
Stefan Blanke <blanke@enterio.de>
2024-03-04 15:05:37 +0100

    WIP Replace Flying Saucer with OpenHtmlToPdf

--- src/main/java/com/spraed/flyingsaucer/PDFGenerator.java
+++ src/main/java/com/spraed/flyingsaucer/PDFGenerator.java
@@ -1,85 +1,51 @@
 package com.spraed.flyingsaucer;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
-import org.w3c.dom.DOMException;
-import org.w3c.tidy.Tidy;
-import org.xhtmlrenderer.pdf.ITextFontResolver;
-import org.xhtmlrenderer.pdf.ITextRenderer;
 
-import com.lowagie.text.DocumentException;
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+import org.w3c.tidy.Tidy;
 
 public class PDFGenerator {
 
-	public static void main(String[] args) throws IOException, DocumentException, ParseException {
+	public static void main(String[] args) throws IOException, ParseException {
 		CommandLine cmd = defineOptions(args);
 
-		InputStream tmpInputStream = new FileInputStream(cmd.getOptionValue("html"));
-		InputStream dataStream = new DataInputStream(tmpInputStream);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(dataStream));
-
-		List<String> htmlFiles = new ArrayList<String>();
-		String strLine;
-		while ((strLine = reader.readLine()) != null) {
-			htmlFiles.add(strLine);
-		}
-
-		reader.close();
+		String htmlContent = readHtmlFiles(cmd.getOptionValue("html"), cmd.getOptionValue("encoding"));
 
 		OutputStream os = new FileOutputStream(cmd.getOptionValue("pdf"));
-		ITextRenderer renderer = new ITextRenderer();
 
-		String fontPaths = cmd.getOptionValue("fontPaths");
-		if (fontPaths != null) {
-			configureFonts(renderer, fontPaths.split(","));
-		}
-
-		int index = 0;
-		for (String htmlInputFile : htmlFiles) {
-			File htmlOutputFile = tidyHtml(htmlInputFile, cmd.getOptionValue("encoding"));
-
-			try {
-				renderer.setDocument(htmlOutputFile);
-				renderer.layout();
-
-				if (index == 0) {
-					renderer.createPDF(os, false);
-				} else {
-					renderer.writeNextDocument();
-				}
-				index++;
-			} finally {
-				htmlOutputFile.delete();
+		try {
+			PdfRendererBuilder rendererBuilder = new PdfRendererBuilder();
+			String fontPaths = cmd.getOptionValue("fontPaths");
+			if (fontPaths != null) {
+				configureFonts(rendererBuilder, fontPaths.split(","));
 			}
+
+			rendererBuilder.withHtmlContent(htmlContent, null);
+			rendererBuilder.toStream(os);
+			rendererBuilder.run();
+		} finally {
+			os.close();
 		}
-
-		renderer.finishPDF();
-		os.close();
-
 	}
 
-	protected static void configureFonts(ITextRenderer renderer, String[] fontPaths)
-			throws DOMException, DocumentException, IOException {
-
-		ITextFontResolver resolver = renderer.getFontResolver();
-
+	private static void configureFonts(PdfRendererBuilder rendererBuilder, String[] fontPaths) {
 		for (String fontPath : fontPaths) {
-			resolver.addFont(fontPath, true);
+			rendererBuilder.useFont(new File(fontPath), "MyFont");
 		}
 	}
 
@@ -91,15 +57,38 @@ public class PDFGenerator {
 		options.addOption("fontPaths", true, "CSV font paths");
 
 		CommandLineParser parser = new PosixParser();
-		CommandLine cmd = parser.parse(options, args);
-
-		return cmd;
+		return parser.parse(options, args);
 	}
 
-	private static File tidyHtml(String htmlInputFile, String encoding) throws IOException {
-		InputStream htmlInputStream = new FileInputStream(htmlInputFile);
-		File htmlOutputFile = File.createTempFile("tidy", ".html");
-		OutputStream htmlOutputStream = new FileOutputStream(htmlOutputFile);
+	private static String readHtmlFiles(String htmlListFilePath, String encoding) throws IOException {
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(new FileInputStream(htmlListFilePath), encoding));
+		String line;
+		StringBuilder sb = new StringBuilder();
+		while ((line = reader.readLine()) != null) {
+			String htmlFilePath = line.trim();
+			String htmlContent = readHtmlFile(htmlFilePath, encoding);
+			sb.append(htmlContent);
+		}
+		reader.close();
+
+		return tidyHtml(sb.toString(), encoding);
+	}
+
+	private static String readHtmlFile(String htmlFilePath, String encoding) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(htmlFilePath), encoding));
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			sb.append(line).append("\n");
+		}
+		reader.close();
+
+		return tidyHtml(sb.toString(), encoding);
+	}
+
+	private static String tidyHtml(String htmlContent, String encoding) {
+		htmlContent = htmlContent.replaceAll("<!DOCTYPE[^>]*>", "");
 
 		Tidy tidy = new Tidy();
 		tidy.setXHTML(true);
@@ -107,14 +96,12 @@ public class PDFGenerator {
 		tidy.setShowWarnings(false);
 		tidy.setQuiet(true);
 		tidy.setForceOutput(true);
-
 		tidy.setInputEncoding(encoding);
 		tidy.setOutputEncoding(encoding);
-		tidy.parse(htmlInputStream, htmlOutputStream);
 
-		htmlInputStream.close();
-		htmlOutputStream.close();
+		StringWriter writer = new StringWriter();
+		tidy.parse(new StringReader(htmlContent), writer);
 
-		return htmlOutputFile;
+		return writer.toString();
 	}
 }
